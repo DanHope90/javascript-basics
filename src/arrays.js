fbc758d8287acd8245ef48594325c219c8bf11c9
Dan Hope <dplhope@gmail.com>
2021-12-03 11:53:37 +0000

    All tests complete an ready for review

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,67 @@
 const getNthElement = (index, array) => {
-  // your code here
+
+  return array[index % array.length]; 
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.join(',');
 };
 
 const csvStringToArray = string => {
-  // your code here
+ return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  return array.concat(element);
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+
+  return numbers.map(numbers => numbers.toString());
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  return strings.map(strings => strings.toUpperCase(','));
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  return strings.map(strings => strings.split('').reverse().join(''));
 };
 
 const onlyEven = numbers => {
-  // your code here
+  return numbers.filter(number => (number % 2) === 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
-};
+  return array.slice(0, 2).concat(array.slice(3)) ; 
+}
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  return strings.filter(string => /^[aeiou]/i.test(string));
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.split(" ").join("");
 };
 
 const sumNumbers = numbers => {
-  // your code here
+ return numbers.reduce((acc, number) => {
+   return acc + number;
+ })
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  return strings.sort((a, b) => 
+  a.charCodeAt(a.length -1) - b.charCodeAt(b.length -1));
+
 };
 
 module.exports = {
--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,50 @@
 const createPerson = (name, age) => {
-  // your code here
+return {name, age};
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object.age;
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
-};
+  return person.age > 65
+  };
 
 const getAges = people => {
-  // your code here
+  return people.map(people => people.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(people => people.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+ return cars.filter(cars => cars.manufacturer === 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+ const totalAge = people.reduce((accAge, eachPerson) => {
+  return accAge + eachPerson.age;
+}, 0);
+  return totalAge / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name: name,
+    age: age,
+    introduce: introducePerson => {
+    return `Hi Fred, my name is ${name} and I am ${age}!`;
+   }
+  };  
 };
 
 module.exports = {
